61495bc643766306414b15f7cedabb0211fed1c3
nyyakko <nyakonyns@gmail.com>
2026-01-09 10:08:12 -0300

    atualizar pesos

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import Prioriza from './Prioriza';
 import CadastrarAcao from './Pages/Acao/CadastrarAcao';
 import EditarAcao from './Pages/Acao/EditarAcao';
 
-localStorage.setItem('pesos', JSON.stringify({ iest: 0.556, iset: 0.244, ifin: 0.155, igov: 0.043 }));
+// localStorage.setItem('pesos', JSON.stringify({ iest: 0.556, iset: 0.244, ifin: 0.155, igov: 0.043 }));
+// localStorage.setItem('pesos', JSON.stringify({ iest: 0.505, iset: 0.259, ifin: 0.194, igov: 0.040 }));
+localStorage.setItem('pesos', JSON.stringify({ iest: 0.474, iset: 0.257, ifin: 0.224, igov: 0.043 }));
 
 const routes = [
     { path: '/prioriza', element: <Prioriza /> },
