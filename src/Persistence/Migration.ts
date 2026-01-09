19fb111c18c74f2536cddd6ad807597ae4e14961
nyyakko <nyakonyns@gmail.com>
2026-01-09 19:54:23 -0300

    hack: por enquanto, recarregar a janela para aguardar o banco migrar é suficiente.

--- src/Persistence/Migration.ts
+++ src/Persistence/Migration.ts
@@ -4,4 +4,6 @@ connection.onupgradeneeded = () => {
     const companies = connection.result?.createObjectStore('companies', { keyPath: 'id', autoIncrement: true });
     companies.createIndex('by_id', 'id');
     companies.createIndex('by_ticker', 'stock.ticker');
+
+    window.location.reload();
 };
